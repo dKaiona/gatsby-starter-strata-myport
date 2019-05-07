5c9e12b9a8d388393d930f1b8e88f29f7877cdb3
dKaiona <14drob35@gmail.com>
2019-05-07 11:35:25 -0600

    Header first push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import me from '../assets/images/me.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <a href="#" className="image avatar"><img src={me} alt="" /></a>
+                    <h1><strong>Daniel</strong>, I am<br />
+                    <strong>A</strong> Team Player<br/>
+                     <strong>A</strong> Web Devloper<br />
+                      <strong>A</strong> Novice Fisherman<br/>
+                    </h1>
                 </div>
                 <Footer />
             </header>
